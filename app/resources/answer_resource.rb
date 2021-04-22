83e583e4d25518e4a4fdc97005527ac3e8df15b6
wilberC <wilber@able.co>
2021-04-21 22:16:26 -0500

    add-camel-case-to-responses

--- app/resources/answer_resource.rb
+++ app/resources/answer_resource.rb
@@ -1,5 +1,7 @@
 class AnswerResource < JSONAPI::Resource
-  attributes :question_id, :questionnaire_id, :url_response
+  attributes :question_id, :questionnaire_id, :url_response, :is_correct
   has_one :question
   has_one :questionnaire
+
+  filter :questionnaire_id
 end
--- app/resources/questionnaire_resource.rb
+++ app/resources/questionnaire_resource.rb
@@ -1,6 +1,8 @@
 class QuestionnaireResource < JSONAPI::Resource
-  attributes :user_id, :number_of_questions, :correct_answers, :incorrect_answers
+  attributes :user_id, :number_of_questions, :correct_answers, :incorrect_answers, :created_at
   has_many :questions
   has_many :answers
   has_one :user
+
+  filter :user_id
 end
